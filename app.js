940c45e17dae890e114d108c4328c7f076615e52
Pedro Fortuna <DeathZonePT@users.noreply.github.com>
2016-03-15 10:32:07 +0000

    Just my start! :D
    i'M REALLY GOING DEEP ON THIS :D

---
+++ app.js
@@ -0,0 +1,21 @@
+const electron = require('electron')
+const app = electron.app;
+const BrowserWindow = require('electron').BrowserWindow;
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+mainWindow = null;
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({ 
+	width: 1024, 
+	height: 768, 
+	center: true, 
+	frame: false
+ })
+
+  // mainWindow.loadURL('file://' + __dirname + '/browser.html');
+  mainWindow.loadURL('https://youtube.com/');
+});
